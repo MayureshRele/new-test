dc9c7670933fa1b91bbf742792686ed2143f2678
HuNtErX6969 <mayureshrele@gmail.com>
2022-03-19 13:20:38 +0530

    fileds

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,43 +1,58 @@
 <template>
-  <v-dialog persistent v-model="isModal" :id="id">
-    <v-card width="500">
-      <v-card-title>
-        <span class="text-h5">{{ title }}</span>
-      </v-card-title>
-      <v-card-text>
-        <v-container>
-          <v-row no-gutters>
-            <v-col cols="12" class="p-0">
-              <Input
-                label="name"
-                type="text"
-                :value="productsController.name"
-              />
-            </v-col>
-            <v-col cols="12" class="p-0">
-              <Input
-                label="description"
-                type="text"
-                :value="productsController.description"
-              />
-            </v-col>
-            <v-col cols="12" class="p-0">
-              <Input
-                label="image"
-                type="text"
-                :value="productsController.image"
-              />
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-card-text>
-      <v-card-actions>
-        <v-spacer></v-spacer>
-        <v-btn color="blue darken-1" text @click="closeModal"> Close </v-btn>
-        <v-btn color="blue darken-1" text @click="dialog = false"> Save </v-btn>
-      </v-card-actions>
-    </v-card>
-  </v-dialog>
+  <div class="formContainer">
+    <v-dialog persistent v-model="isModal" :id="id">
+      <v-card width="500">
+        <v-card-title>
+          <span class="text-h5">{{ title }}</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container>
+            <v-row no-gutters>
+              <v-col cols="12" class="p-0">
+                <Input
+                  v-model="productController.product.name"
+                  type="text"
+                  label="Name"
+                  :value="productController.product.name"
+                />
+              </v-col>
+              <v-col cols="12" class="p-0">
+                <Input
+                  v-model="productController.product.description"
+                  type="text"
+                  label="Description"
+                  :value="productController.product.description"
+                />
+              </v-col>
+              <v-col cols="12" class="p-0">
+                <Input
+                  v-model="productController.product.image"
+                  type="text"
+                  label="Image"
+                  :value="productController.product.image"
+                />
+              </v-col>
+              <v-col cols="12" class="p-0">
+                <Input
+                  v-model="productController.product.price"
+                  type="text"
+                  label="Price"
+                  :value="productController.product.price"
+                />
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" text @click="closeModal"> Close </v-btn>
+          <v-btn color="blue darken-1" text @click="dialog = false">
+            Save
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
 </template>
 
 <script lang="ts" >
@@ -64,20 +79,30 @@ export default defineComponent({
       return store.getters.Ismodal;
     });
 
-    const productsController = reactive({
-      name: "",
-      description: "",
-      image: "",
-    });
     const closeModal = () => {
       emitter.emit("close");
       store.dispatch(Actions.IS_MODAL, { isOpen: false });
     };
 
+    const productController = reactive({
+      product: {
+        email: "",
+        name: "",
+        description: "",
+        image: "",
+        price: "",
+        qty: 0,
+      },
+      submit() {
+        debugger;
+        emitter.emit("submit", productController.product);
+      },
+    });
+
     return {
       isModal,
       closeModal,
-      productsController,
+      productController,
     };
   },
 });
